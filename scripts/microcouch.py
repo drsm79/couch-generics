9fc3f89a83adeb17b5bd7a2aef214535f3ec4d8f
Simon Metson <simon+github@cloudant.com>
2012-04-20 14:27:23 +0100

    MicroCouch class
    To make authenticated things easier

--- scripts/microcouch.py
+++ scripts/microcouch.py
@@ -1,8 +1,10 @@
+import getpass
 import base64
 import json
 import urllib
 import urllib2
 from urlparse import urlunparse, urlparse
+
 
 class PutRequest(urllib2.Request):
     def get_method(self):
@@ -11,70 +13,87 @@ class PutRequest(urllib2.Request):
 class HeadRequest(urllib2.Request):
     def get_method(self):
         return "HEAD"
-
-def push(docs, database):
+class MicroCouch:
     """
-    Push a list of docs to the database
+    An ugly class that provides minimal interactions with CouchDB
     """
 
-    parts = urlparse(database)
-    if parts.port:
-        netloc = '%s:%s' % (parts.hostname, parts.port)
-    else:
-        netloc = parts.hostname
+    def __init__(self, database):
+        parts = urlparse(database)
 
-    url_tuple = (
-            parts.scheme,
-            netloc,
-            parts.path,
-            parts.params,
-            parts.query,
-            parts.fragment
-            )
-    url = urlunparse(url_tuple)
+        self.auth = False
+        self.created = False
 
-    try:
-        urllib2.urlopen(PutRequest(url))
-    except:
-        # The data base probably already exists
-        pass
+        if parts.port:
+            netloc = '%s:%s' % (parts.hostname, parts.port)
+        else:
+            netloc = parts.hostname
 
-    def get_rev(doc):
+        url_tuple = (
+                parts.scheme,
+                netloc,
+                parts.path,
+                parts.params,
+                parts.query,
+                parts.fragment
+                )
+        self.url = urlunparse(url_tuple)
+        self.do_auth(parts)
+        self.create_db()
+
+    def do_auth(self, parts):
+        if parts.username and parts.password:
+            auth_tuple = (parts.username, parts.password)
+            self.auth = base64.encodestring('%s:%s' % auth_tuple).strip()
+        elif parts.username:
+            auth_tuple = (parts.username, getpass.getpass())
+            self.auth = base64.encodestring('%s:%s' % auth_tuple).strip()
+
+    def create_db(self):
+        try:
+            req = PutRequest(self.url)
+            if self.auth:
+                req.add_header("Authorization", "Basic %s" % self.auth)
+            urllib2.urlopen(req)
+            self.created = True
+        except Exception, e:
+            # The data base probably already exists
+            print e
+            pass
+
+    def push(self, docs):
         """
-        Get the _rev for a doc if it has one, and add to the doc
+        Push a list of docs to the database
         """
-        if '_id' in doc.keys():
-            docid = doc['_id']
-            # HEAD the doc
-            docurl = "%s/%s" % (url, urllib.quote_plus(docid))
-            try:
-                head = urllib2.urlopen(HeadRequest(docurl))
-                # get its _rev, append _rev to the doc dict
-                doc['_rev'] = head.info().dict['etag'].strip('"')
-            except urllib2.HTTPError, e:
-                pass
-        return doc
+        def get_rev(doc):
+            """
+            Get the _rev for a doc if it has one, and add to the doc
+            """
+            if '_id' in doc.keys():
+                docid = doc['_id']
+                # HEAD the doc
+                docurl = "%s/%s" % (self.url, urllib.quote_plus(docid))
+                try:
+                    head = urllib2.urlopen(HeadRequest(docurl))
+                    # get its _rev, append _rev to the doc dict
+                    doc['_rev'] = head.info().dict['etag'].strip('"')
+                except urllib2.HTTPError, e:
+                    pass
+            return doc
 
-    docs = map(get_rev, docs)
+        if not self.created:
+            # if the database didn't exist the docs won't either!
+            docs = map(get_rev, docs)
 
-    auth = False
+        req = urllib2.Request('%s/_bulk_docs' % self.url)
+        req.add_header("Content-Type", "application/json")
 
-    if parts.username and parts.password:
-        auth_tuple = (parts.username, parts.password)
-        auth = base64.encodestring('%s:%s' % auth_tuple).strip()
-    elif parts.username:
-        auth_tuple = (parts.username, getpass.getpass())
-        auth = base64.encodestring('%s:%s' % auth_tuple).strip()
+        if self.auth:
+            req.add_header("Authorization", "Basic %s" % self.auth)
 
-    req = urllib2.Request('%s/_bulk_docs' % url)
-    req.add_header("Content-Type", "application/json")
-
-    if auth:
-        req.add_header("Authorization", "Basic %s" % auth)
-
-    data = {'docs': docs}
-    req.add_data(json.dumps(data))
-    f = urllib2.urlopen(req)
-    f.read()
-    f.close()
+        data = {'docs': docs}
+        req.add_data(json.dumps(data))
+        f = urllib2.urlopen(req)
+        f.read()
+        f.close()
 
